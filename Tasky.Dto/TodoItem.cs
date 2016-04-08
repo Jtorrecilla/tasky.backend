bdb178fbe308dc221464516b10aa1893d08e66aa
Jtorrecilla <rastagar2003@gmail.com>
2016-04-08 21:04:58 +0200

    initial

---
+++ Tasky.Dto/TodoItem.cs
@@ -0,0 +1,10 @@
+﻿
+namespace tktTodoAppService.Dto
+{
+    public class TodoItem : EntityData
+    {
+        public string Text { get; set; }
+
+        public bool Complete { get; set; }
+    }
+}
